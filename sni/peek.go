ad56f0c32e63eb297d252ef5c6858ff256c2a60c
dipghoshraj <dipghoshraj@gmail.com>
2026-01-18 23:28:50 +0530

    fallback to non tls

--- sni/peek.go
+++ sni/peek.go
@@ -1,29 +1,55 @@
 package sni
 
 import (
+	"fmt"
 	"log"
 	"net"
 )
 
+// func PeekSNI(conn net.Conn) (string, net.Conn, error) {
+// 	buf := make([]byte, 512)
+// 	n, err := conn.Read(buf)
+// 	if err != nil {
+// 		return "", nil, err
+// 	}
+// 	serverName, err := SniStream(buf[:n])
+// 	if err != nil {
+// 		log.Println("[SNI] error on found the with TLS servername", err)
+// 		serverName, err = ExtractHostFromStream(buf[:n])
+// 		if err != nil {
+// 			log.Println("[SNI] error on found the servername", err)
+// 		}
+// 	}
+
+// 	log.Printf("found the server name domain: %s", serverName)
+
+// 	if err != nil {
+// 		return "", nil, err
+// 	}
+// 	return serverName, &ConnBuffer{buf: buf[:n], conn: conn}, nil
+// }
+
 func PeekSNI(conn net.Conn) (string, net.Conn, error) {
-	buf := make([]byte, 512)
-	n, err := conn.Read(buf)
-	if err != nil {
-		return "", nil, err
-	}
-	serverName, err := SniStream(buf[:n])
-	if err != nil {
-		log.Println("[SNI] error on found the with TLS servername", err)
-		serverName, err = ExtractHostFromStream(buf[:n])
+	var buf []byte
+	tmp := make([]byte, 4096)
+
+	for {
+		n, err := conn.Read(tmp)
 		if err != nil {
-			log.Println("[SNI] error on found the servername", err)
+			return "", nil, err
+		}
+
+		buf = append(buf, tmp[:n]...)
+
+		sni, err := SniStream(buf)
+		if err == nil && sni != "" {
+			log.Printf("found server name: %s", sni)
+			return sni, &ConnBuffer{buf: buf, conn: conn}, nil
+		}
+
+		// Prevent unbounded buffering
+		if len(buf) > 64*1024 {
+			return "", nil, fmt.Errorf("ClientHello too large or SNI not found")
 		}
 	}
-
-	log.Printf("found the server name domain: %s", serverName)
-
-	if err != nil {
-		return "", nil, err
-	}
-	return serverName, &ConnBuffer{buf: buf[:n], conn: conn}, nil
 }
